e6ec4b414d48dc10d03a155f7b4c7a40f7c4606a
jonelle-noelani <jonelle.noelani@gmail.com>
2020-09-20 03:04:44 -0700

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,35 @@
+class StudentsController < ApplicationController
+    before_action :find, only: [:show, :edit, :update]
+    
+    def index
+        @students = Student.All 
+    end
+
+    def show
+    end
+
+    def new
+        @student = Student.new
+    end
+
+    def create
+        @student = Student.create(params.require(:student).permit(:first_name, :last_name))
+        redirect_to student_path(@student)
+    end
+
+    def edit
+    end
+
+    def update
+        @student.update(params.require(:student).permit(:first_name, :last_name))
+        redirect_to student_path(@student)
+    end
+
+    private
+
+    def find
+        @student = Student.find(params[:id])
+    end
+    
+end
+        
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ActiveRecord::Base
-  def to_s
+  def whole_name
     self.first_name + " " + self.last_name
   end
 end
